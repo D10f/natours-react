7d5b5fe728c7a0bd4f0ac408d900d5986a1cfec1
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-02-09 22:39:57 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+import { render } from 'react-dom';
+import Header from './scripts/components/Header';
+
 import './styles.scss';
+
+const App = () => {
+  return (
+    <Header />
+  )
+};
+
+render(<App />, document.getElementById('root'));
