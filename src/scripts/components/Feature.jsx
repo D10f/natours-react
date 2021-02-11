fbfccc005cd3fbcd5209cd0d95bec539be69888b
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-02-11 19:33:35 +0100

    fixed icons and update browserslistrc

--- src/scripts/components/Feature.jsx
+++ src/scripts/components/Feature.jsx
@@ -1,6 +1,6 @@
-const Feature = ({ title, description }) => (
+const Feature = ({ title, description, icon }) => (
   <article className="feature">
-    <i className="feature__icon icon-basic-world has-background"></i>
+    <i className={`feature__icon has-background ${icon}`}></i>
     <h3 className="feature__heading heading-tertiary small-margin-bottom">{title}</h3>
     <p className="feature__description paragraph">{description}</p>
   </article>
--- src/scripts/sections/Features.jsx
+++ src/scripts/sections/Features.jsx
@@ -3,19 +3,23 @@ import Feature from '../components/Feature';
 const featureData = [
   {
     title: 'Explore The World',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    icon: 'icon-basic-world'
   },
   {
     title: 'Meet Nature',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    icon: 'icon-basic-compass'
   },
   {
     title: 'Find Your Way',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    icon: 'icon-basic-map'
   },
   {
     title: 'Live A Healthier Life',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    icon: 'icon-basic-heart'
   }
 ];
 
@@ -25,9 +29,9 @@ const Features = () => {
       <div className="row large-margin-top">
 
         {
-          featureData.map(({ title, description }, idx) => (
+          featureData.map(({ title, description, icon }, idx) => (
             <div className="column" key={Math.random()}>
-              <Feature title={title} description={description} />
+              <Feature title={title} description={description} icon={icon} />
             </div>
           ))
         }
