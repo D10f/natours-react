cd8d709214bd1851c9bd9ed023015ce91f960896
herokunt <6wdcd9q17d86.scruff@simplelogin.co>
2021-02-12 02:14:30 +0100

    fix issue with chrome

--- src/scripts/components/SectionHeader.jsx
+++ src/scripts/components/SectionHeader.jsx
@@ -1,6 +1,6 @@
 const SectionHeader = ({ sectionTitle }) => (
-  <header className="section-header has-background">
-    <h2 className="section-header__headline medium-margin-bottom">{sectionTitle}</h2>
+  <header className="section-header">
+    <h2 className="section-header__headline has-background medium-margin-bottom">{sectionTitle}</h2>
   </header>
 );
 
